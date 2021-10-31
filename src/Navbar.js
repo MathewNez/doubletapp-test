59d741c387253ba28fc9e9a3b18324e40a4ce9d5
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-01 00:11:13 +0500

    Added a Navbar component

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg"
+const Navbar = () =>  {
+    return(
+        <nav className="navbar">
+            <img src={ logo } alt={ logo }/>
+            <h1>STUDENTS by M3f3w_1337</h1>
+        </nav>
+
+    );
+}
+export default Navbar;
