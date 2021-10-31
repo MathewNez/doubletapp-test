9233ec80f2dde9afab2862b17a3c7348f366a4d0
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-01 00:27:43 +0500

    Added Students component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Navbar";
+import Students from "./Students";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+        <div className="content">
+            <Students />
+        </div>
 
     </div>
   );
---
+++ src/Students.js
@@ -0,0 +1,7 @@
+const Students = () => {
+    return (
+        <h2>Students will be displaying here, I guess</h2>
+    );
+}
+
+export default Students;
