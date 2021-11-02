e9c7b455d10b22afc366d6fb5a7de36e8543cf44
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-03 00:03:45 +0500

    Made right spacing for columns

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,22 @@
 import DeleteButton from "./DeleteButton";
+import logo from "./logo.svg"
 
 const Table = () => {
     return(
         <table>
+            <colgroup>
+                <col span="1" style={{width: '80px'}}/>
+                <col span="1" style={{width: '341px'}}/>
+                <col span="1" style={{width: '320px'}}/>
+                <col span="1" style={{width: '160px', padding: '0 0 60px 0'}}/>
+                <col span="1" style={{width: '200px', padding: '0 0 60px 0'}}/>
+                <col span="1" style={{width: '162px'}}/>
+                <col span="1" style={{width: '82px'}}/>
+                <col span="1" style={{width: '68px'}}/>
+            </colgroup>
             <thead>
                 <tr>
+                    <th/>
                     <th>ФИО</th>
                     <th>Специальность</th>
                     <th>Группа</th>
@@ -14,21 +26,23 @@ const Table = () => {
             </thead>
             <tbody>
                 <tr>
+                    <td><img src={logo} alt={logo}/></td>
                     <td>Пупа</td>
                     <td>Крыса с моаиса</td>
                     <td>МО-1337</td>
                     <td>Много</td>
-                    <td>Ты мне мозги-то не еби, я спрашиваю: Сколько?</td>
-                    <td>Цвет тут должен потом быть</td>
+                    <td>322</td>
+                    <td>цвт</td>
                     <td><DeleteButton /></td>
-            </tr>
+                </tr>
                 <tr>
+                    <td><img src={logo} alt={logo}/></td>
                     <td>Лупа</td>
                     <td>фиит (сосёт)</td>
                     <td>ФТ-Артемий Рогоффффф</td>
                     <td>74</td>
                     <td>228</td>
-                    <td>Цвет тут должен потом быть</td>
+                    <td>Цвт</td>
                     <td><DeleteButton /></td>
                 </tr>
             </tbody>
