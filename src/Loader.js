e342037990e7c0c36281a299e04090d896af6445
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-03 21:04:57 +0500

    Added a loading message if loading

---
+++ src/Loader.js
@@ -0,0 +1,11 @@
+const Loader = () => {
+    return(
+        <div className="container2">
+            {/*<div className="loader"/>*/}
+            <div className="loading">Загружаем данные, пожалуйста, подождите...</div>
+        </div>
+
+    );
+}
+
+export default Loader;
--- src/Table.js
+++ src/Table.js
@@ -1,35 +1,53 @@
 import {useEffect, useState} from "react";
 import StudentsList from "./StudentsList";
+import Loader from "./Loader";
 
 const Table = () => {
 
     const [students, setStudents] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {fetchData();}, []);
-    const fetchData = async () => {
-        const response = await fetch("https://front-assignment-api.2tapp.cc/api/persons");
-        const parsed = await response.json();
-        setStudents(parsed.students);
-    }
+
+    // useEffect( () => {
+    //     fetch("https://front-assignment-api.2tapp.cc/api/persons")
+    //         .then(result => result.json())
+    //         .then((parsed) => {
+    //             setStudents(parsed.students);
+    //             setIsLoading(false);
+    //         });
+    // }, []);
+
+    useEffect( () => {
+        setTimeout(() => {
+        fetch("https://front-assignment-api.2tapp.cc/api/persons")
+            .then(result => result.json())
+            .then((parsed) => {
+                setStudents(parsed.students);
+                setIsLoading(false);
+            });
+    }, 1000)}, []);
+
 
     const handleDelete = (id) => {
         const newStudents = students.filter((student) => student.id !== id);
         setStudents(newStudents);
     }
 
-    return(
-        <table>
-            <colgroup>
-                <col span="1" style={{width: '80px'}}/>
-                <col span="1" style={{width: '341px'}}/>
-                <col span="1" style={{width: '320px'}}/>
-                <col span="1" style={{width: '160px', padding: '0 0 60px 0'}}/>
-                <col span="1" style={{width: '200px', padding: '0 0 60px 0'}}/>
-                <col span="1" style={{width: '162px'}}/>
-                <col span="1" style={{width: '82px'}}/>
-                <col span="1" style={{width: '68px'}}/>
-            </colgroup>
-            <thead>
+    return (
+        <div>
+            {
+            <table>
+                <colgroup>
+                    <col span="1" style={{width: '80px'}}/>
+                    <col span="1" style={{width: '341px'}}/>
+                    <col span="1" style={{width: '320px'}}/>
+                    <col span="1" style={{width: '160px', padding: '0 0 60px 0'}}/>
+                    <col span="1" style={{width: '200px', padding: '0 0 60px 0'}}/>
+                    <col span="1" style={{width: '162px'}}/>
+                    <col span="1" style={{width: '82px'}}/>
+                    <col span="1" style={{width: '68px'}}/>
+                </colgroup>
+                <thead>
                 <tr>
                     <th/>
                     <th>ФИО</th>
@@ -38,11 +56,14 @@ const Table = () => {
                     <th>Возраст</th>
                     <th>Рейтинг</th>
                 </tr>
-            </thead>
-            <tbody>
-               <StudentsList students={students} handleDelete={handleDelete} />
-            </tbody>
-        </table>
+                </thead>
+                {isLoading && <Loader />}
+                <tbody>
+                <StudentsList students={students} handleDelete={handleDelete}/>
+                </tbody>
+            </table>
+            }
+        </div>
     );
 }
 
