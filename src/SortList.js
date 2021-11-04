65ebbc0bc644e6e05f2d719d9d329707c14e68e2
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-04 23:09:42 +0500

    Made color changing when option is selected

--- src/SortList.js
+++ src/SortList.js
@@ -8,7 +8,7 @@ const SortList = ({handleSort}) => {
     const DropDownHeader = styled("div")`
       position: relative;
       display: flex;
-      min-width: 180px;
+      width: 180px;
 
       /* White */
       background: #FFFFFF;
@@ -34,7 +34,7 @@ const SortList = ({handleSort}) => {
       /* 1 */
       box-shadow: 0 7px 64px rgba(0, 0, 0, 0.07);
       border-radius: 6px;
-      min-width: 180px;
+      width: 180px;
     `;
     const ListItem = styled("li")`
       list-style: none;
@@ -44,8 +44,10 @@ const SortList = ({handleSort}) => {
       font-size: 12px;
       line-height: 15px;
       margin-bottom: 4px;
-      height: 35px;
-      padding: 12px 0 7px 18px;
+      height: 16px;
+      padding: 10px 0 7px 18px;
+      border-radius: 5px;
+      background: ${props => props.option === props.selected ? 'rgba(73, 194, 232, 0.11)' : 'none'};
     `;
     const ListIcon = styled("img")`
       width: 17px;
@@ -65,13 +67,13 @@ const SortList = ({handleSort}) => {
     const onOptionClicked = value => () => {
         setSelectedOption(value);
         setIsOpen(false);
-        handleSort(selectedOption);
+        handleSort(value);
     };
 
     return (
         <DropDownContainer>
             <DropDownHeader onClick={toggling}>
-                Имя А-Я
+                {selectedOption || sortOptions[0]}
                 <ListIcon src={ddIcon}/>
             </DropDownHeader>
             {isOpen && (
@@ -79,7 +81,7 @@ const SortList = ({handleSort}) => {
                     <DropDownList>
                         {sortOptions.map((option, index) => {
                             return (
-                                <ListItem onClick={onOptionClicked(option)} key={index}>
+                                <ListItem onClick={onOptionClicked(option)} key={index} option={option} selected={selectedOption}>
                                     {option}
                                 </ListItem>);
                         })}
