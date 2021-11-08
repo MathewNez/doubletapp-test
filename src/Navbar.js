05d016ab9c068c4631e06b9bc77c04e7bfa06b31
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-08 12:56:06 +0500

    Added links to a navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,8 @@ import logo from "./logo.svg"
 const Navbar = () =>  {
     return(
         <nav className="navbar">
-            <img src={ logo } alt={ logo }/>
-            <h1>STUDENTS by <code>M3f3w_1337</code></h1>
+            <a href="/"> <img src={ logo } alt={ logo }/> </a>
+            <h1>STUDENTS by <code><a href="https://github.com/MathewNez">MathewNez</a></code></h1>
         </nav>
 
     );
