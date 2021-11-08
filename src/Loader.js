2cf0f1ab20841d5f7bd5bc00823a927b7775d250
Mathew Neznaeff <matveyneznaev@mail.ru>
2021-11-08 12:33:23 +0500

    Removed useless div

--- src/Loader.js
+++ src/Loader.js
@@ -1,7 +1,6 @@
 const Loader = () => {
     return(
         <div className="container2">
-            {/*<div className="loader"/>*/}
             <div className="loading">Загружаем данные, пожалуйста, подождите...</div>
         </div>
 
